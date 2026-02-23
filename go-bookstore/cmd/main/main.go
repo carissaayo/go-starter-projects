ecb9c00c21647320a2841763987503a51d2ff9a9
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-02-23 15:48:22 +0100

    booking

--- go-bookstore/cmd/main/main.go
+++ go-bookstore/cmd/main/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/carissayo/go-beginner-projects/bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+
+	log.Fatal(http.ListenAndServe(":9010", r))
+}
