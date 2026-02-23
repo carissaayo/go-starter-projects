02073b837c0fef956ea5114b1cdee1129b6c6007
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-02-23 13:03:33 +0100

    okay

--- go-movies-crud/cmd/main.go
+++ go-movies-crud/cmd/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
@@ -21,5 +25,18 @@ var movies []Movie
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc()
+	movies = append(movies, Movie{ID: "1", Isbn: "4322333", Title: "Movie One", Director: &Director{Firstname: "John", Lastname: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "4322332", Title: "Movie Two", Director: &Director{Firstname: "Jane", Lastname: "Doe"}})
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Stating erver at port  8000\n")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
