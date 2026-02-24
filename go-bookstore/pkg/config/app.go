89662228b1c4c69c64d3f477d75ca6828ec412fd
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-02-24 10:18:17 +0100

    done with this

--- go-bookstore/pkg/config/app.go
+++ go-bookstore/pkg/config/app.go
@@ -10,7 +10,10 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "postgres:postgres/bookstore?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open(
+		"mysql",
+		"root:root@tcp(127.0.0.1:3306)/bookstore?charset=utf8&parseTime=True&loc=Local",
+	)
 
 	if err != nil {
 		panic(err)
