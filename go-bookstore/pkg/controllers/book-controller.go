644228da228c974b4320f377eb89ff2a7fa55725
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-02-24 09:38:26 +0100

    writing controller

--- go-bookstore/pkg/controllers/book-controller.go
+++ go-bookstore/pkg/controllers/book-controller.go
@@ -1,1 +1,36 @@
 package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/carissayo/go-beginner-projects/bookstore/pkg/models"
+	"github.com/gorilla/mux"
+)
+
+var NewBook models.Book
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks()
+
+	res, _ := json.Marshal(newBooks)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader((http.StatusOK))
+	w.Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+
+	bookDetails, _ := models.GetBookById(ID)
+}
